403e58c81f370123d124c3e05dfd23e09502dc8e
Chhin Satya <chhinsatya097@gmail.com>
2024-01-09 16:28:18 +0700

    Test commit

---
+++ eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  extends: ["airbnb", "prettier", "prettier/react"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": [1, {
+      "extensions": [".js", ".jsx"]
+    }]
+  }
+};
